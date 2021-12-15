eb7c262107584f558888dc0e5b6e5eca0688b52e
JEROMEBRANCHETTI <jeromebranchetti00@gmail.com>
2021-12-15 17:43:03 +0100

    primo commit

---
+++ db_university_test_script.sql
@@ -0,0 +1,57 @@
+USE `db_university`;
+
+-- 1. Selezionare tutti gli studenti nati nel 1990
+SELECT * 
+FROM students s
+WHERE YEAR(date_of_birth) < 2000;
+
+-- 2.Selezionare tutti i corsi che valgono più di 10 crediti
+SELECT *
+FROM courses c 
+WHERE c.cfu > 9;
+
+-- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT *
+FROM students s 
+WHERE YEAR(date_of_birth) < 1991;
+
+-- 4. Selezionare tutti i corsi di laurea magistrale
+SELECT *
+FROM `degrees` d 
+WHERE d.`level` = 'magistrale';
+
+-- 5. Da quanti dipartimenti è composta l’università?
+SELECT COUNT(d.id) 
+FROM departments d;
+
+-- 6. Quanti sono gli insegnanti che non hanno un numero di telefono?
+SELECT COUNT(t.id) as prof_senza_numero
+FROM teachers t 
+WHERE t.phone IS NULL;
+
+-- 7. Contare quanti iscritti ci sono stati ogni anno
+SELECT YEAR(s.enrolment_date) , COUNT(s.id)
+FROM students s 
+GROUP BY YEAR(s.enrolment_date)
+ORDER BY YEAR(s.enrolment_date);
+
+
+
+-- 8. Calcolare la media dei voti di ogni appello d’esame
+SELECT es.exam_id,  AVG(es.vote)
+FROM exam_student es 
+GROUP BY es.exam_id 
+ORDER BY es.exam_id;
+
+-- 9. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+
+-- 10. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+-- 11. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+
+-- 12. Selezionare tutti i corsi in cui insegna Fulvio Amato
+
+-- 13. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+
